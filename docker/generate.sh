78dc7d70fc778022b369f98aa2a1648997244915
Konstantin Belyalov <arsenicus@mail.ru>
2020-01-19 22:16:45 -0800

    Revert google.any - nanopb generated code does not look nice

--- docker/generate.sh
+++ docker/generate.sh
@@ -9,6 +9,6 @@ mkdir -p go/openiot nanopb
 cd proto
 
 # Generate golang code
-find . -name "*.proto" | xargs -L1 protoc --go_out=../go/openiot
+find . -name "*.proto" | xargs -L1 protoc --go_out=../go
 # Generate C code (nanopb)
 find . -name "*.proto" | xargs -L1 protoc --plugin=protoc-gen-nanopb=/nanopb/generator/protoc-gen-nanopb --nanopb_out=../nanopb
